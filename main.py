673c1732230a154945dd428147003801a1e318a1
Kostia2004 <k.a.kudriavsev@mail.ru>
2022-03-02 20:16:09 +0300

    add scan choice radio

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def matrixToMpl(dct):
 
     plt.subplot(133)
     plt.imshow(ct, cmap = 'bone')
-    plt.imshow(res, alpha=0.5, cmap = 'bone')
+    plt.imshow(res, alpha=0.7, cmap = 'bone')
     plt.title('predicted infection mask')
 
     return fig
@@ -44,18 +44,35 @@ st.subheader("")
 
 st.subheader("Нейросеть, сегментирующая области, пораженные COVID-19 на снимках КТ легких")
 st.write('[Github проекта](https://github.com/Kostia2004/CovidSegmentation)')
-uploaded_scan = st.file_uploader("Choose nii file", type=['nii'])
-testScanPath = 'testfiles/radiopaedia_org_covid-19-pneumonia-29_86491_1-dcm.nii'
-arr = None
-if uploaded_scan is None:
-    reader = Reader() 
+scan_choice = st.radio("Select scan for predict", ("Upload", "First test", "Second test", "Third test"))
+
+print(st.session_state)
+
+if scan_choice=="Upload":
+    uploaded_scan = st.file_uploader("Choose nii file", type=['nii'])
+
+test1 = 'testfiles/radiopaedia_org_covid-19-pneumonia-10_85902_1-dcm.nii'
+test2 = 'testfiles/radiopaedia_org_covid-19-pneumonia-29_86491_1-dcm.nii'
+test3 = 'testfiles/radiopaedia_org_covid-19-pneumonia-4_85506_1-dcm.nii'
+
+if scan_choice!="Upload":
+    reader = Reader()
+    testScanPath = ""
+    match scan_choice:
+        case "First test":
+            testScanPath = test1
+        case "Second test":
+            testScanPath = test2
+        case "Third test":
+            testScanPath = test3
+
     arr = reader.read(testScanPath)
     segmenter = Segmenter()
-if ('result' not in st.session_state) and ('lung' not in st.session_state) and ('ct' not in st.session_state):
-    st.session_state.result, st.session_state.lung, st.session_state.ct = segmenter.segmentation(arr)
-if 'alllist' not in st.session_state:
-    st.session_state.alllist = [{'result': list(st.session_state.result)[i][...,0], 'lung': list(st.session_state.lung)[i][...,0], 'ct': list(st.session_state.ct)[i][...,0]} for i in range(arr.shape[2])]
-if 'figlist' not in st.session_state:
-    st.session_state.figlist = list(map(matrixToMpl, st.session_state.alllist))
-slicenum = st.slider("Number of slice", 1, len(st.session_state.alllist))
-st.pyplot(st.session_state.figlist[slicenum])
+    if ('result' not in st.session_state) and ('lung' not in st.session_state) and ('ct' not in st.session_state):
+        st.session_state.result, st.session_state.lung, st.session_state.ct = segmenter.segmentation(arr)
+    if 'alllist' not in st.session_state:
+        st.session_state.alllist = [{'result': list(st.session_state.result)[i][...,0], 'lung': list(st.session_state.lung)[i][...,0], 'ct': list(st.session_state.ct)[i][...,0]} for i in range(arr.shape[2])]
+    if 'figlist' not in st.session_state:
+        st.session_state.figlist = list(map(matrixToMpl, st.session_state.alllist))
+    slicenum = st.slider("Number of slice", 1, len(st.session_state.alllist))
+    st.pyplot(st.session_state.figlist[slicenum])
